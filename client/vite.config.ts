2d1eb37ab2e11bc4dd8d4809a9cf37bb56687930
Bablu Mia <154756232+bablumia1@users.noreply.github.com>
2024-02-11 10:15:38 +0600

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: "https://graphql-listing-server.vercel.app",
         changeOrigin: true,
         secure: false,
       },
